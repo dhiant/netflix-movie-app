084f8eb4c9c01a81ceaa46040a4228d43d066270
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-06-13 11:46:06 +0545

    feat: add list item

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -33,16 +33,16 @@ const List = () => {
           style={{ display: !displayArrow && "none" }}
         />
         <div className="container" ref={listRef}>
-          <ListItem index="1" />
-          <ListItem index="2" />
-          <ListItem index="3" />
-          <ListItem index="4" />
-          <ListItem index="5" />
-          <ListItem index="6" />
-          <ListItem index="7" />
-          <ListItem index="8" />
-          <ListItem index="9" />
-          <ListItem index="10" />
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
         </div>
         <IoIosArrowDropright
           className="slideArrow right"
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,36 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./listItem.scss";
 import { BsFillPlayFill } from "react-icons/bs";
 import { RiAddFill } from "react-icons/ri";
 import { BiLike, BiDislike } from "react-icons/bi";
 
 const ListItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const videoURL = `https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761`;
   return (
-    <div className="listItemWrapper">
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img
         src="https://cdn.onebauer.media/one/empire-images/features/5cde8a00133d503e3a49e503/GOT-MAIN.jpg?format=jpg&quality=80&width=960&height=540&ratio=16-9&resize=aspectfill"
         alt=""
       />
-      <div className="itemInfo">
-        <div className="icons">
-          <BsFillPlayFill />
-          <RiAddFill />
-          <BiLike />
-          <BiDislike />
-        </div>
-        <div className="itemInfoTop">
-          <span>1 hour 14mins</span>
-          <span className="limit">+16</span>
-          <span>1999</span>
-        </div>
-        <div className="movieDes">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-          deserunt quisquam, placeat expedita molestiae illum excepturi aliquid
-          ex quidem saepe, aspernatur perspiciatis ratione! Assumenda quae quos
-          magni harum ad aliquam.
-        </div>
-        <div className="genre">Action</div>
-      </div>
+      {isHovered && (
+        <>
+          <video src={videoURL} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <BsFillPlayFill className="icon" />
+              <RiAddFill className="icon" />
+              <BiLike className="icon" />
+              <BiDislike className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Praesentium hic rem eveniet error possimus, neque ex doloribus.
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
