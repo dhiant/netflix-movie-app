60cd5b063b0f04a54fe2594d747788258f58284b
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-06-14 23:34:24 +0545

    feat: add login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.scss";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      {/* <Home /> */}
+      {/* <Watch /> */}
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
