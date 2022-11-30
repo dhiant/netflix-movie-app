4ced9b4c7014cc1a6ed8ab991c1dca8b8cb5fc7b
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-11-30 14:22:59 +0545

    add: connect node to mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection is successful"))
+  .catch((err) => {
+    console.log("error occurs", err);
+  });
+
+mongoose.connection.on("error", (err) => {
+  console.log("errors after initial connection", err);
+});
 
 app.listen(8800, () => {
-  console.log("backend server is running");
+  console.log("backend server is running on port 8800");
 });
