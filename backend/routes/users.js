7127b01e38dc1dc229c393cba1e7296b4d84e47a
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-12-05 22:21:15 +0545

    add: delete a user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -22,11 +22,33 @@ router.put("/:id", verifyToken, async (req, res) => {
         // return new user
         { new: true }
       );
+      res.status(200).json(updateUser);
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
     res.status(403).json("You can update only your account.");
+  }
+});
+
+// delete
+router.delete("/:id", verifyToken, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASSWORD_KEY
+      ).toString();
+    }
+
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can delete only your account.");
   }
 });
 
