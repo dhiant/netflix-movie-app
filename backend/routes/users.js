9508f75dab955dfd204341e053900547e1c60b47
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-12-05 22:36:17 +0545

    add: get single or multiple users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -52,4 +52,32 @@ router.delete("/:id", verifyToken, async (req, res) => {
   }
 });
 
+// get single user
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...userInfo } = user._doc;
+    res.status(200).json(userInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get all or new users(last 10 users)
+router.get("/", verifyToken, async (req, res) => {
+  const query = req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(10)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed to see all users!");
+  }
+});
+
 module.exports = router;
